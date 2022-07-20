2ecdf02611087fcee0b6cc9632bc57a8f32c73a5
Black Jiang <blackj@mega7.fun>
2022-07-20 14:18:49 +0800

    feat: 調整build版 dependencies

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,9 @@
 import Params from './util/params.js'
 import { fileRebuilder } from './util/file-rebuilder/index.js'
 
-const main = () => {
+const main = async () => {
   console.log('Starting build web entry.')
-  const params = Params()
+  const params = await Params()
   
   fileRebuilder({ params })
   console.log('Building web entry success.')
--- lib/util/params.ts
+++ lib/util/params.ts
@@ -4,20 +4,20 @@ import {
 } from './normalize.js'
 import { generateNewEntryFilePath } from './generator.js'
 
-const argv = await yargs(process.argv.slice(2))
-  .option('nginx-path', {
-    desc: 'Nginx config file path.',
-    type: 'string',
-    default: 'nginx.conf',
-  })
-  .option('entry-file-path', {
-    desc: 'Entry filename.',
-    type: 'string',
-    alias: 'entry',
-    default: 'dist/index.html',
-  }).argv
+export default (async () => {
+  const argv = await yargs(process.argv.slice(2))
+    .option('nginx-path', {
+      desc: 'Nginx config file path.',
+      type: 'string',
+      default: 'nginx.conf',
+    })
+    .option('entry-file-path', {
+      desc: 'Entry filename.',
+      type: 'string',
+      alias: 'entry',
+      default: 'dist/index.html',
+    }).argv
 
-export default (() => {
   const params: Params = (() => {
     const entryFilePath = normalizePath({ path: argv.entryFilePath })
     const { fullPath, fileName } = generateNewEntryFilePath({ entryFilePath })
