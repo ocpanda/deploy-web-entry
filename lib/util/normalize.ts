33eb83a0bed3b96fb6d76f96534f683f641c5e5e
Black Jiang <blackj@mega7.fun>
2022-07-20 16:13:13 +0800

    fix: 修正pathValidate

--- lib/util/normalize.ts
+++ lib/util/normalize.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 
 const pathValidate: Validate = path => {
-  const regex = /^\w+(\/\w+)*\.(htm(l*)|conf)$/g
+  const regex = /^(\/)?\w+(\/\w+)*\.(htm(l*)|conf)$/g
 
   const validated = regex.test(path)
   if (!validated) chalk.yellow(`
@@ -16,7 +16,6 @@ const normalizeFolderPath: NormalizePath<{
 }> = ({ path }) => {
   path = path
     .replace(/(\\)+/g, '/')
-    .replace(/^\//g, '')
     .replace(/\/$/g, '')
     .replace(/(\/)+/g, '/')
 
